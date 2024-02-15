ba22422458c8b78d5bb2dd7524440b238741ed77
Ruiz <pr19556@appliedmed.com>
2024-02-14 17:43:29 -0800

    big swag 14feb

--- betaboard.py
+++ betaboard.py
@@ -6,49 +6,72 @@ from datetime import datetime
 import matplotlib.pyplot as plt 
 import os 
 import keyboard 
+import os
 
-def read_voltage(port, time_lim, time_step):
+def read_values(port, time_lim, time_step):
     t1, t2 = time.time(), time.time() 
-    voltage = []
+    values = []
     
     while(t2 - t1) <= time_lim:
         time.sleep(time_step)
         t2 = time.time()
-        voltage.append([t2-t1, port.read()])
+        values.append(port.read())
     
-    return np.array(voltage)
+    return np.array(values)
 
 def csv_export(data):
-    data_df = pd.DataFrame(data, columns = ["Time", "Voltage"])
     save_path = os.getcwd() + "\\BetaBoard"
     file_name = "ForceData_" + str(datetime.now().strftime("%Y-%m-%d %H-%M-%S")) + ".csv"
     file_path = save_path + "\\" + file_name
-    data_df.to_csv(file_path, index = False)
-    print(data_df)
+    data.to_csv(file_path, index = False)
 
-def main(time_lim, time_step):
-    board = Arduino("COM3")
+def main(comport, time_lim, time_step, savefile):
+    os.system('cls')
+    
+    board = Arduino(comport)
     iterator = util.Iterator(board)
     iterator.start()
 
     data_df = pd.DataFrame()
 
+    print("\n**************************************\n\n ----- BetaBoard FSR Testing ----- \n\n**************************************\n")
+    print("Press ENTER to continue . . . \n")
+    keyboard.wait("enter")
+
+    os.system('cls')
+    print(f"Connected to: {comport}\n")
+    print("----- Test Parameters ----- ")
+    print(f"Time per Sample: {str(float(time_lim))} (sec)")
+    print(f"Readings per Sample: {str(int(time_lim/time_step))}")
+    print("----------------------------\n")
+
+    test_num, data = 1, {}
+
+    print("Press SPACE to begin test . . .")
+    keyboard.wait("space")
     while True:
         try:
-            print("Press SPACE to begin test.")
-            print(f"Time Limit: {str(float(time_lim, 2))}")
-            print(f"Time Step: {str(float(time_step, 2))}")
+            os.system("cls")
+            print(f"-------- Test Num: {str(int(test_num))} --------\n")
+            print(f"Test Num {str(int(test_num))} . . . In Progress\n")
+            result = read_values(board.analog[2], time_lim, time_step)
+            new_data = {str(test_num): result}
+            data.update(new_data)
+            print(f"Test Num {str(int(test_num))} . . . Complete\n")
+            print("-----------------------------\n")
+            test_num = test_num + 1
+            print("Press SPACE to begin next test . . .")
+            print("Press CTRL+C to end testing . . .")
             keyboard.wait("space")
-
-            result = read_voltage(board.analog[2], time_lim, time_step)
-            print(result)
         
         except KeyboardInterrupt:
-            csv_export(data_df)
-            print(data_df)
+            data_df = pd.DataFrame.from_dict(data)
+            if savefile == True:
+                csv_export(data_df)
+            
+            print(f"\nDisconnected from {comport}\n")
             break
-        break
 
 if __name__ == "__main__":
-    main(10, 1)
+    main("COM4", 1, .2, False)
 
